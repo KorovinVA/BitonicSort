8a9902418f9b5f23b72dd4227aa4a6fd15fa4ab3
Korovin Vlad <korovin.va@phystech.edu>
2021-09-29 21:17:50 +0300

    [core] Implemented bitonic sort

--- src/core/bitonicsort.hpp
+++ src/core/bitonicsort.hpp
@@ -9,8 +9,9 @@
 #include <CL/sycl.hpp>
 
 template <typename T> class BitonicSort {
+    typedef unsigned long long ull;
+
     const std::vector<T> &m_input;
-    const std::vector<T> &m_gold;
     std::vector<T> &m_output;
 
     const sycl::device m_device;
@@ -18,8 +19,7 @@ template <typename T> class BitonicSort {
 
   public:
     BitonicSort(const std::vector<T> &input, std::vector<T> &output,
-                const sycl::device_selector &device = sycl::default_selector(),
-                const std::vector<T> &gold = std::vector<T>());
+                const sycl::device_selector &device = sycl::default_selector());
     BitonicSort(const BitonicSort &) = delete;
 
     void Run();
@@ -31,15 +31,11 @@ template <typename T> class BitonicSort {
 template <typename T>
 inline BitonicSort<T>::BitonicSort(const std::vector<T> &input,
                                    std::vector<T> &output,
-                                   const sycl::device_selector &device,
-                                   const std::vector<T> &gold)
+                                   const sycl::device_selector &device)
     : m_input(input), m_output(output), m_device(sycl::device(device)),
-      m_gold(gold), m_inSize(input.size()) {
+      m_inSize(input.size()) {
     assert(m_inSize != 0);
     assert(m_input.size() == m_output.size());
-    if (!m_gold.empty()) {
-        assert(m_input.size() == m_gold.size());
-    }
 }
 
 template <typename T> inline void BitonicSort<T>::Run() {
@@ -47,27 +43,63 @@ template <typename T> inline void BitonicSort<T>::Run() {
 
     // If array size is not 2^n, add values to the array
     // to have 2^n elements.
-    if ((m_inSize & (m_inSize - 1)) != 0) {
+    size_t dataSize = m_output.size();
+    if ((dataSize & (dataSize - 1)) != 0) {
         T maxVal = std::numeric_limits<T>::max();
-        size_t newSize = std::pow(2, std::ceil(std::log(m_inSize) / log(2)));
-        for (size_t i = 0; i < newSize - m_inSize; ++i) {
+        size_t newSize = std::pow(2, std::ceil(std::log(dataSize) / log(2)));
+        for (size_t i = 0; i < newSize - dataSize; ++i) {
             m_output.push_back(maxVal);
         }
     }
+
+    std::cout << "DEVICE: " << m_device.get_info<sycl::info::device::name>()
+              << std::endl;
     RunInParallel();
 }
 
 template <typename T> inline void BitonicSort<T>::RunInParallel() {
-    std::cout << "DEVICE: " << m_device.get_info<sycl::info::device::name>()
-              << std::endl;
-
+    ull numSteps = std::ceil(std::log(m_output.size()) / log(2));
     auto range = sycl::range(m_output.size());
-    sycl::buffer buf(m_output.data(), range);
-    sycl::queue q(m_device);
+    auto ndRange = sycl::nd_range<>(range, range);
 
+    sycl::buffer<T, 1> buff(m_output.data(), m_output.size());
+    sycl::buffer<ull, 1> sNum(&numSteps, 1);
+
+    sycl::queue q(m_device);
     q.submit([&](auto &h) {
-        sycl::accessor arr(buf, h, sycl::read_write);
-        h.parallel_for(range, [=](cl::sycl::id<1> wiID) {;
+        sycl::accessor data(buff, h, sycl::read_write);
+        sycl::accessor steps(sNum, h, sycl::read_only);
+
+        h.parallel_for(ndRange, [=](cl::sycl::nd_item<1> wiID) {
+            const auto BitonicStep = [&](ull callDepth, ull nodeHeight) {
+                ull globalID = wiID.get_global_id();
+                ull numWarpsInCmp = 1 << (nodeHeight - 1);
+                ull numWarpsInSeq = numWarpsInCmp << (callDepth + 1);
+
+                bool isWarpComparing = (globalID / numWarpsInCmp) % 2 == 0;
+                bool isIncreasingCmp =
+                    isWarpComparing && ((globalID / numWarpsInSeq) % 2 == 0);
+
+                if (isWarpComparing) {
+                    if (isIncreasingCmp) {
+                        if (data[globalID + numWarpsInCmp] < data[globalID])
+                            std::swap(data[globalID],
+                                      data[globalID + numWarpsInCmp]);
+
+                    } else {
+                        if (data[globalID] < data[globalID + numWarpsInCmp])
+                            std::swap(data[globalID],
+                                      data[globalID + numWarpsInCmp]);
+                    }
+                }
+            };
+
+            for (ull i = 1; i <= steps[0]; ++i) {
+                for (ull j = i; j > 0; --j) {
+                    BitonicStep(i - j, j);
+                    wiID.barrier();
+                }
+            }
         });
     });
 }
--- src/core/main.cpp
+++ src/core/main.cpp
@@ -1,13 +1,13 @@
 #include <vector>
 #include "bitonicsort.hpp"
 
-#define SIZE 16
+#define SIZE 17
 
 int main() {
-    std::vector<int> in(SIZE);
+    std::vector<int> in = {7, 16, 5 , 8, 9, 10, 11, 13, 8, 4, 5, 9, 15, 13, 0, 1, 1};
     std::vector<int> out(SIZE);
 
-    auto sort = BitonicSort<int>(in, out, sycl::cpu_selector());
+    auto sort = BitonicSort<int>(in, out, sycl::gpu_selector());
     sort.Run();
 
     for (auto it: out) {
